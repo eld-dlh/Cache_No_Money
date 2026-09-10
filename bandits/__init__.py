7bd69a198669644b5383017dff4bf5244a35735e
danushvenkat-git <1dt24cs064@dsatm.edu.in>
2026-09-10 15:56:07 +0530

    Add UCB1 bandit

--- bandits/__init__.py
+++ bandits/__init__.py
@@ -1,5 +1,7 @@
 from .toy_env import ToyBanditEnv
 from .epsilon_greedy import EpsilonGreedy
+from .ucb1 import UCB1
 
-__all__ = ["ToyBanditEnv", "EpsilonGreedy"]
+__all__ = ["ToyBanditEnv", "EpsilonGreedy", "UCB1"]
+
 
